1b7d25d34e33b158ab38d50f0093b6b468e44e03
ANKIT JHA <ankitjha01aj@gmail.com>
2022-04-01 23:59:57 +0530

    changed json

---
+++ build/precache-manifest.6b99406f1dfa21b319db6a13d4bffdc3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3e4e853ab799d26da5c5156a043e79",
+    "url": "/react-live/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/react-live/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "587638701b0583f2d283",
+    "url": "/react-live/static/js/main.c5bc6622.chunk.js"
+  },
+  {
+    "revision": "108c2d062937e0db831f",
+    "url": "/react-live/static/js/runtime-main.a8a12716.js"
+  }
+]);
